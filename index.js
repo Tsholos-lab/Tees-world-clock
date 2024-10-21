44e0357ef04095de860b037abfba0c57c4c23451
Tsholos-lab <masegompshe@icloud.com>
2024-10-21 09:13:19 +0200

    added changes on html and js file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateTime() {
     let johannesburgTimeElement = johannesburgElement.querySelector(".time");
     let johannesburgTime = moment().tz("Africa/johannesburg");
 
-    johannesburgElement.innerHTML = johanneburgTime.format("MMMM	Do YYYY");
+    johannesburgDateElement.innerHTML = johanneburgTime.format("MMMM	Do YYYY");
     johanneburgTimeElement.innerHTML = johannesburgTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
@@ -27,19 +27,16 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
     <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+  </div>
+  <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
   </div>
